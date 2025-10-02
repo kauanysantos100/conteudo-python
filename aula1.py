8e9d0ce7fbb68fde28e12418ce5cfd1b55c3c78c
KAUANY HADASSA DOS SANTOS <kauany_h_santos@estudante.sesisenai.org.br>
2025-10-02 16:48:43 -0300

    conteudo sobre print e variaveis

---
+++ aula1.py
@@ -0,0 +1,12 @@
+#como nosso primeiro código, vamos escapar da maldição
+print("Olá mundo!")
+#Abaixo está o comando print com sep e end padrão
+print ("Lógica", "de","programação",sep=" ",end="\n")
+#Abixo cada concatenação é um entr (\n significa enter)
+print ("Vai","imprimir","em"," baixo?",sep="\n")
+#Abaixo cada comando posuio end alterado para imprimir na mesma linha
+print("Vai",end=" ")
+print("imprimir",end=" ")
+print("na",end=" ")
+ print("mesma",end=" ")
+ print("linha",end=" ")
---
+++ aula1_variaveis.py
@@ -0,0 +1,12 @@
+#Podemos  definir variáveis com o nome dela, seguido pelo
+#sinal de igual, que significa "atribuir' e depois pelo valor.
+#nome = "Kauany" <- Crie uma variavel nome  que recebe o valor "Kauany"
+
+
+nome= "Kauany"
+sálario = 10000000.00
+dias_trabalhados = 22
+print ("O funionário é",nome,"(tipo: ", type)
+print("Ele trabalhou",dias_trabalhados,"dias")
+print("o sálario é",sálario)
+
