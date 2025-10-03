acf4f68f8c4a29d1d68de3731a2ec8761d39e925
KAUANY HADASSA DOS SANTOS <kauany_h_santos@estudante.sesisenai.org.br>
2025-10-03 16:55:36 -0300

    exercicios

---
+++ exercicios/sequencial/exercicio_3.py
@@ -0,0 +1,6 @@
+numero_1 = int(input("Digite o primeiro valor: "))
+numero_2 = int(input(" Digite o segundo valor: "))
+
+resultado = numero_1 + numero_2
+print("A soma do numero 1 com numero 2 é: ",resultado)
+
---
+++ exercicios/sequencial/exercicio_4.py
@@ -0,0 +1,11 @@
+nota_1 = float(input("Digite a primeira nota: "))
+nota_2 = float(input("Digite a segundo nota: "))
+nota_3 = float(input ("Digite a terceira nota: "))
+nota_4 = float(input("Digite a quarta nota "))
+
+soma = nota_1 + nota_2 + nota_3 + nota_4
+media = soma/ 4
+
+media_outro = (nota_1 + nota_2 + nota_3 + nota_4)
+
+print(" A media de aluno é:")
