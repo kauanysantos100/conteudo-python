1132199bac5458e1d738c78a3b2c5468fadc7b39
KAUANY HADASSA DOS SANTOS <kauany_h_santos@estudante.sesisenai.org.br>
2025-10-03 16:51:21 -0300

    Aula 2: variaveis, input e calculos

---
+++ aula2.py
@@ -0,0 +1,27 @@
+# Para criar uma variavel, colocamos:
+# o nome dela
+# o simbolo de atribuiçao (=)
+# o valor inicial dela
+
+# Uma variavel chamada nome que recebe o meu nome
+nome = "Kauany"
+print("olá, me chamo",nome)
+print("O tipo de variavel é",type(nome))
+nome = "Hadassa"
+print("Agora meu nome é",nome)
+print()
+#Uma variavel do tipo inteiro
+quantidades_alunos = 16
+print("Estamos em",quantidades_alunos,"alunos na sala")
+print("O tipo de variavel é",type(quantidades_alunos))
+print()
+#Uma variavel do tipo float
+salario_bruto = 1425.23
+print("O salário mínimo é R$",salario_bruto)
+print("O tipo de variavel é",type(salario_bruto))
+print()
+#Uma variavel o tipo bool
+is_aberta = False
+print("A porta esta´aberta?",is_aberta)
+print("O tipo da variavel é",type(is_aberta))
+
---
+++ aula2_calculos.py
@@ -0,0 +1,35 @@
+# Arquivo para aprender a realizar cálculos com Python
+
+# Sinais matemáticos:
+# + Soma                        Serve para somar valores
+# - Subtração                   Serve para diminuir um valor do outro
+# * Multiplicação               Serve para multiplicar valores
+# / Divisão                     Serve para dividir um valor para outro
+# ** Potência                   Serve para fazer a potência de valores
+# // Divisão inteira            Serve para retornar a parte inteira da divisão
+# % Resto de divisão            Serve para retornar o resto da divisão
+
+numero_1 = int(input("Digite o primeiro valor: "))
+numero_2 = int(input(" Digite o segundo valor: "))
+
+resultado = numero_1 + numero_2
+print("A soma do numero 1 com numero 2 é: ",resultado)
+
+resultado = numero_1 - numero_2
+print("A subtração do numero 1 pelo numero 2 é: ",resultado)
+
+resultado = numero_1 * numero_2
+(" A multiplicação do numero 1 pelo numero 2 é",resultado)
+
+resultado = numero_1 / numero_2
+print("A divisão do numero 1 pelo numero 2 é",resultado)
+
+resultado = numero_1 ** numero_2
+print(" A potência do numero 1 pelo numero 2 é",resultado)
+
+resultado = numero_1 // numero_2
+print("A divisão inteira do numero 1 pelo numero 2 é",resultado)
+
+resultado = numero_1 % numero_2
+print("O resto da divisão do numero 1 pelo numero 2 é",resultado)
+
